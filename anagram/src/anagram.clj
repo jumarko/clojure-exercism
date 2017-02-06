fc27b156481b746dbdacfbe6b662c561da434a90
Juraj Martinka <juraj@snapshot.travel>
2017-02-06 09:45:46 +0100

    Clean up anagram exercise by extracting helper function and using thread-last macro

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -1,16 +1,17 @@
 (ns anagram
   (:require [clojure.string :as s]))
 
+(defn- frequencies-match? [word]
+  (let [word-frequencies (frequencies (s/lower-case word))]
+    (fn  [[w f]] (and
+                   (not= (s/lower-case w) (s/lower-case word))
+                   (= f word-frequencies)))))
+
 (defn anagrams-for
   "Given the word and list of potential anagrams return a sublits of `candidates`
    which contains only true anagrams of given `word`"
   [word candidates]
-  (let [word-frequencies (frequencies (s/lower-case word))
-        candidates-frequencies (map (fn [word] [word (frequencies (s/lower-case  word))]) candidates)]
-    (map
-      (fn [[w f]] w)
-      (filter
-        (fn [[w f]] (and
-                      (not= (s/lower-case w) (s/lower-case word))
-                      (= f word-frequencies)))
-        candidates-frequencies))))
+  (let [candidates-frequencies (map (fn [word] [word (frequencies (s/lower-case  word))]) candidates)]
+    (->> candidates-frequencies
+      (filter (frequencies-match? word))
+      (map (fn [[w f]] w)))))
