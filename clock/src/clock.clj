f350873f482044cb135fd4bfec9c1503afbbb248
Juraj Martinka <juraj@snapshot.travel>
2017-02-18 11:29:28 +0100

    Clock exercise: clock->string implementation

---
+++ clock/src/clock.clj
@@ -0,0 +1,21 @@
+(ns clock)
+
+;;; TODO: what's the best clock representation?
+;;; vector?
+;;; map?
+;;; defrecord?
+
+(defn clock
+  "Clock that process time without date"
+  [hour minute]
+  [hour minute])
+
+(defn clock->string
+  "convers clock to human-redable string representation "
+  [[hour minute]]
+  ;; check format documentation and examples: https://clojuredocs.org/clojure.core/format
+  (format "%02d:%02d" hour minute))
+
+(defn add-time
+  "Adds or substracts (if negative) hours and minutes from given clock."
+  [clock hour minute])
