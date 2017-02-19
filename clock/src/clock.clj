c75c546b304026d6598f7ac07ad9cf19ad0dfdff
Juraj Martinka <jumarko@gmail.com>
2017-02-19 07:21:02 +0100

    Clock: make clock roll over even if minutes are negative.

--- clock/src/clock.clj
+++ clock/src/clock.clj
@@ -1,15 +1,20 @@
 (ns clock)
 
-;;; TODO: what's the best clock representation?
-;;; vector?
-;;; map?
-;;; defrecord?
+(defn- minutes->hours
+  "Converts minutes that 'roll over' to hours.
+   E.g. 81 minutes should be converted to 1 hour.
+   -124 minutes should be converted to -2 hours."
+  [minutes]
+  (let [div (quot minutes 60)]
+    (if (neg? minutes)
+      (dec div)
+      div)))
 
 (defn clock
   "Clock that process time without date"
   [hour minute]
-  (let [minutes->hours (quot minute 60)
-        hours (mod (+ hour minutes->hours) 24)
+  (let [minutes-hours (minutes->hours minute)
+        hours (mod (+ hour minutes-hours) 24)
         minutes (mod minute 60)]
     [hours minutes]))
 
@@ -21,4 +26,4 @@
 
 (defn add-time
   "Adds or substracts (if negative) hours and minutes from given clock."
-  [clock hour minute])
+  [clock minute])
