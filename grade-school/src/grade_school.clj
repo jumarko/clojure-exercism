007b935cb720ad9e01d4ec0276beaa92286de0e2
Juraj Martinka <jumarko@gmail.com>
2017-02-21 06:21:50 +0100

    Grade school exercise.

--- grade-school/src/grade_school.clj
+++ grade-school/src/grade_school.clj
@@ -6,3 +6,16 @@
   ;; inspiration to use fnil: http://stackoverflow.com/questions/35328702/if-key-exists-update-otherwise-assoc
   (update db grade (fnil conj []) student))
 
+
+(defn grade
+  "Returns all students in given grade."
+  [db grade]
+  (get db grade []))
+
+(defn sorted
+  "Returns students for each grade in ascending order by grades while ordering students in grade alphabetically."
+  [db]
+  (->> db
+       (map (fn [[k v]] [k (sort v)]))
+       (into (sorted-map))))
+
