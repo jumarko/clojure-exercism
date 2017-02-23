0f4d0e45f2c5f84f2c34aed18a8b4b810fb5e512
Juraj Martinka <jumarko@gmail.com>
2017-02-23 06:25:20 +0100

    First version of Robot Name exercise.

--- robot-name/src/robot_name.clj
+++ robot-name/src/robot_name.clj
@@ -6,13 +6,26 @@
 (defn- random-digit []
   (rand-nth (range 10)))
 
-(defn robot-name
+(defn- random-name
   []
   (apply str
          [(random-char) (random-char) (random-digit) (random-digit) (random-digit)]))
 
+(defn reset-name
+  [robot]
+  (reset! robot {::name (random-name)}))
+
+(defn robot-name
+  [robot]
+  (::name @robot))
+
 (defn robot
   "Creates new robot with factory settings"
   []
-  {::name (random-name)})
+  (atom {::name (random-name)}))
+
+(def robot1 (robot))
+(robot-name robot1)
+(reset-name robot1)
+(robot-name robot1)
 
