8abffefa7ebdf7b304d36ee376ac89b570ba0be3
Juraj Martinka <jumarko@gmail.com>
2017-02-28 06:00:15 +0100

    ETL transform exercise

--- etl/src/etl.clj
+++ etl/src/etl.clj
@@ -5,9 +5,11 @@
   to new formart [Letter -> Points]"
   [legacy-mapping]
   (->> legacy-mapping
-       (group-by (fn [[k v]] v))
-       (map (fn [[points words]] [points (vec (map first words))]))
-       (into {})))
+       (map (fn [[points words]] (zipmap
+                                  (map clojure.string/lower-case words)
+                                  (repeat (count words) points))))
+       (into {})
+       ))
 
-(transform {"apple" 1 "artichoke" 1 "boat" 2 "ballerina" 2})
+(transform {1 ["APPLE" "ARTICHOKE"], 2 ["BOAT" "BALLERINA"]})
 
