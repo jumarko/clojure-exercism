f2e01e83331b299d6a7ba435936f911d978db2b9
Juraj Martinka <jumarko@gmail.com>
2017-02-28 06:06:10 +0100

    ETL transform: extract private function to make code more readable.

--- etl/src/etl.clj
+++ etl/src/etl.clj
@@ -1,15 +1,17 @@
 (ns etl)
 
+(defn- points->words [[points words]]
+  (zipmap
+   (map clojure.string/lower-case words)
+   (repeat (count words) points)))
+
 (defn transform
   "Transforms legacy scrabble data from mapping [Point -> Letters]
   to new formart [Letter -> Points]"
   [legacy-mapping]
   (->> legacy-mapping
-       (map (fn [[points words]] (zipmap
-                                  (map clojure.string/lower-case words)
-                                  (repeat (count words) points))))
-       (into {})
-       ))
+       (map points->words)
+       (into {})))
 
 (transform {1 ["APPLE" "ARTICHOKE"], 2 ["BOAT" "BALLERINA"]})
 
