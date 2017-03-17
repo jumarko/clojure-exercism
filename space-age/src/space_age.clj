a7e9b8af45899710e4dc12d68c233ce3fe54c034
Juraj Martinka <jumarko@gmail.com>
2017-03-17 10:41:39 +0100

    Space Age: Extract private `convert` function to simplify a code a bit.

--- space-age/src/space_age.clj
+++ space-age/src/space_age.clj
@@ -1,25 +1,28 @@
 (ns space-age)
 
+(defn- convert [seconds planet-to-earth-ration]
+  (/ (on-earth seconds) planet-to-earth-ration))
+
 (defn on-earth [seconds]
   (/ seconds 31557600))
 
 (defn on-mercury [seconds]
-  (/ (on-earth seconds) 0.2408467))
+  (convert seconds 0.2408467))
 
 (defn on-venus [seconds]
-  (/ (on-earth seconds) 0.61519726))
+  (convert seconds 0.61519726))
 
 (defn on-mars [seconds]
-  (/ (on-earth seconds) 1.8808158))
+  (convert seconds 1.8808158))
 
 (defn on-jupiter [seconds]
-  (/ (on-earth seconds) 11.862615))
+  (convert seconds 11.862615))
 
 (defn on-saturn [seconds]
-  (/ (on-earth seconds) 29.447498))
+  (convert seconds 29.447498))
 
 (defn on-uranus [seconds]
-  (/ (on-earth seconds) 84.016846))
+  (convert seconds 84.016846))
 
 (defn on-neptune [seconds]
-  (/ (on-earth seconds) 164.79132))
+  (convert seconds 164.79132))
