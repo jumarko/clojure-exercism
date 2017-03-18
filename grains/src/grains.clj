b11b98709da3d11cb178e2674d7ed08df174a9ac
Juraj Martinka <jumarko@gmail.com>
2017-03-18 18:52:01 +0100

    Grains: reuse lazy sequence created by iterate in both public functions.

--- grains/src/grains.clj
+++ grains/src/grains.clj
@@ -1,19 +1,19 @@
 (ns grains)
 
+(def ^:private lazy-squares
+  (iterate #(bigint (* 2 %)) 1))
+
 (defn square
   "Compute number of grains on given square of chessboard.
   1 grain on the first square, 2 grains on second square, 4 grains on the third one, etc."
   [n]
   ;; notice that we need to cast intermediate result to bigint
   ;; otherwise we'll get overflow exception
-  (nth (iterate #(bigint (* 2 %)) 1)
-       (dec n)))
+  (nth lazy-squares (dec n)))
 
 (defn total
   "Computes total number of grains on the chessboard.
   Check `square` for more details."
   []
-  (->>
-   (range 1 65)
-   (map square)
-   (apply +)))
+  (apply + (take 64 lazy-squares)))
+
