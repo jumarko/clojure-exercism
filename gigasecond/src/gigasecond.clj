aa8fcd4d53cbf2756ec51854eaf5d88faa029829
Juraj Martinka <jumarko@gmail.com>
2017-03-21 06:35:45 +0100

    Gigasecond: Use LocalDateTime instead of ZonedDateTime.
    Timezone has no significance here and using LocalDateTime is more concise.

--- gigasecond/src/gigasecond.clj
+++ gigasecond/src/gigasecond.clj
@@ -1,12 +1,12 @@
 (ns gigasecond
   (:import (java.time.temporal ChronoField)
-           (java.time ZonedDateTime
-                      ZoneOffset)))
+           (java.time LocalDateTime
+                      )))
 
 (defn from
   "Calculates a date which is 1000,000,000 seconds from given date.
   The input and output dates are represented as a vector [year month day]."
   [year month day]
-  (let [start (ZonedDateTime/of year month day 0 0 0 0 ZoneOffset/UTC)
+  (let [start (LocalDateTime/of year month day 0 0 )
         end (.plusSeconds start 1000000000)]
     [(.getYear end) (.getMonthValue end) (.getDayOfMonth end)]))
