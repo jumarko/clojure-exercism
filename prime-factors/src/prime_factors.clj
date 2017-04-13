57da9584f4a27935ba091ee48bd191c58fdf6a8c
Juraj Martinka <jumarko@gmail.com>
2017-04-13 11:07:12 +0200

    Prime factors: optimized version going up to sqrt(n).

--- prime-factors/src/prime_factors.clj
+++ prime-factors/src/prime_factors.clj
@@ -1,20 +1,23 @@
 (ns prime-factors)
 
 (defn of [n]
-  (loop [cur-n n
-         potential-factor 2
-         prime-factors []]
-    (if (< cur-n 2)
-      (if (and (empty? prime-factors) (> n 1))
-        [n]
-        prime-factors)
-      (if (zero? (rem cur-n potential-factor))
-        (recur (quot cur-n potential-factor)
-               potential-factor
-               (conj prime-factors potential-factor))
-        (recur cur-n
-               (inc potential-factor)
-               prime-factors)))))
+  (let [max-factor (int (Math/sqrt n))]
+    (loop [cur-n n
+           potential-factor 2
+           prime-factors []]
+      (if (> potential-factor max-factor)
+
+        (if (> cur-n max-factor)
+          (conj prime-factors cur-n)
+          prime-factors)
+
+        (if (zero? (rem cur-n potential-factor))
+          (recur (quot cur-n potential-factor)
+                 potential-factor
+                 (conj prime-factors potential-factor))
+          (recur cur-n
+                 (inc potential-factor)
+                 prime-factors))))))
 
 (of 1)
 (of 5)
