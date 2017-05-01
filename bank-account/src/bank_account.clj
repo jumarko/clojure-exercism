1f1b8bee7978f8561ab0ed26f0fa7b0141697dfa
Juraj Martinka <jumarko@gmail.com>
2017-05-01 11:29:09 +0200

    Bank account: drastically simplified version using atom that holds a single number.
    My previous version was ok but too complicated for the task at hand.

--- bank-account/src/bank_account.clj
+++ bank-account/src/bank_account.clj
@@ -1,36 +1,23 @@
 (ns bank-account)
 
-(defn- opened? [account]
-  (= :open (:status @account)))
-
-(defmacro ^:private with-opened [account & body]
-  `(when (opened? ~account)
-     ~@body))
-
 (defn open-account
   "Opens given account. This is the first operation starting the account life."
   []
-  (atom
-   {:balance 0
-    :status :open}))
+  (atom 0))
 
 (defn close-account
   "Closes the account. No further operations are allowed on given account,
   that is `nil` is returned by any other operation that will use closed account."
   [account]
-  (do
-    (swap! account assoc :status :close)
-    account))
+  (reset! account nil))
 
 (defn get-balance
   "Returns current balance of the account."
   [account]
-  (with-opened account
-    (:balance @account)))
+  @account)
 
 (defn update-balance
   "Updates balance of given account adding the amount."
   [account amount]
-  (with-opened account
-    (swap! account update :balance (partial + amount))))
+  (swap! account + amount))
 
