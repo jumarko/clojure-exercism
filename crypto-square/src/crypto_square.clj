efd13ca09edf3a3f7aaaad53b56590a51d338ef0
Juraj Martinka <jumarko@gmail.com>
2017-05-04 09:12:46 +0200

    WIP crypto square: plaintext-segments need to be fixed.
    It's way more complicated than it looked like.

--- crypto-square/src/crypto_square.clj
+++ crypto-square/src/crypto_square.clj
@@ -1,4 +1,5 @@
-(ns crypto-square)
+(ns crypto-square
+  (:require [clojure.string :as str]))
 
 (defn normalize-plaintext
   "Normalizes input text by removing all whitespace / punctutation characters
@@ -17,13 +18,35 @@
 (defn plaintext-segments
   "Break input text into segments of size given by `square-size`"
   [s]
-  (let [norm-s (normalize-plaintext s)]
+  (let [norm-s (normalize-plaintext s)
+        col-count (square-size norm-s)
+        diff (- (* col-count col-count) (count norm-s))
+        blocks (if (zero? diff)
+                 (partition col-count norm-s)
+                 (let [row-count (dec col-count)
+                       split-index (* diff row-count)]
+                   (concat
+                    (partition col-count (subs norm-s 0 split-index))
+                    (partition row-count (subs norm-s split-index)))))]
     (mapv (partial apply str)
-          (partition-all (square-size norm-s) norm-s))))
+          blocks)))
+
+(plaintext-segments "Time is an illusion. Lunchtime doubly so.")
 
 (defn ciphertext
   "Encodes given plaintext using crypto sqaure cipher."
-  [s])
+  [s]
+  (->> s
+       plaintext-segments
+       (apply (partial map str))
+       str/join))
+
+(apply (partial map str)
+       ["timeis" "anillu" "sionlu" "nchtim" "edoubl" "yso"])
+
+(subs "123456789abcde" 0 8)
+
+(ciphertext "Time is an illusion. Lunchtime doubly so.")
 
 (defn normalize-ciphertext
   "Breaks encoded text into the blocks using `square-size`"
