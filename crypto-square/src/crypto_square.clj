7ba336d4eca8eff85a86dfe0e8e91f95b96e6127
Juraj Martinka <jumarko@gmail.com>
2017-05-05 13:38:10 +0200

    Crypto Square: normalize-ciphertext is still Work in Progress.

--- crypto-square/src/crypto_square.clj
+++ crypto-square/src/crypto_square.clj
@@ -26,33 +26,36 @@
   "Encodes given plaintext using crypto sqaure cipher."
   [s]
   (let [segments (plaintext-segments s)
-        longest-segment-len (count (first segments))]
+        padding-len (- (count (first segments))
+                       (count (last segments)))]
     (->> segments
-         (map #(concat % (repeat longest-segment-len nil)))
+         ;; since the last segment can have different length then the first ones
+         ;; we need to pad it with the lengt of other segments to make sure
+         ;; that all characters will get to the final result
+         (map #(concat % (repeat padding-len nil)))
          (apply (partial map str))
          str/join)))
 
 (ciphertext "Time is an illusion. Lunchtime doubly so.")
 
+;; TODO: work in progress - need to find a proper way how to split final output
 (defn normalize-ciphertext
-  "Breaks encoded text into the blocks using `square-size`"
+  "Breaks encrypted text into the blocks of `square-size` length.
+  If the encrypted text doesn't fit perfectly into the square, the last n empty spaces
+  are distributed among last n rows."
   [s]
-  (->> (ciphertext s)
-       plaintext-segments
-       (str/join " ")))
+  (let [encrypted   (ciphertext s)
+        row-count   (square-size encrypted)
+        split-index (* row-count
+                       (- row-count
+                          (- (* row-count row-count)
+                             (count encrypted))))
+        first-part  (subs encrypted 0 split-index)
+        second-part (subs encrypted split-index)]
+    (->> (concat
+          (map (partial apply str) (partition-all row-count first-part))
+          (map (partial apply str) (partition-all (dec row-count) second-part)))
+        (str/join " "))
+    ))
 
-#_(defn plaintext-segments
-    "Break input text into segments of size given by `square-size`"
-    [s]
-    (let [norm-s (normalize-plaintext s)
-          col-count (square-size norm-s)
-          diff (- (* col-count col-count) (count norm-s))
-          blocks (if (zero? diff)
-                   (partition col-count norm-s)
-                   (let [row-count (dec col-count)
-                         split-index (* diff row-count)]
-                     (concat
-                      (partition col-count (subs norm-s 0 split-index))
-                      (partition row-count (subs norm-s split-index)))))]
-      (mapv (partial apply str)
-            blocks)))
+(normalize-ciphertext "Time is an illusion. Lunchtime doubly so.")
