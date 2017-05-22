4e88c525dd8ef66a4b4c33d4e21ac857491bdeaf
Juraj Martinka <jumarko@gmail.com>
2017-05-22 09:29:24 +0200

    Accumulate exercise: lazy version.

--- accumulate/src/accumulate.clj
+++ accumulate/src/accumulate.clj
@@ -7,4 +7,11 @@
           []
           coll))
 
-
+;; lazy version
+;; inspired by Daniel Ligth: http://exercism.io/submissions/fe13785f2da7441989f9bf016660c29b
+(defn lazy-accumulate
+  "Transforms each element of given collection with supplied function."
+  [f coll]
+  (lazy-seq
+   (cons (f (first coll))
+         (accumulate f (rest coll)))))
