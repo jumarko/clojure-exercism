d1a2a011bacb3c5756b103d2505d6fb42b7be548
Juraj Martinka <jumarko@gmail.com>
2017-07-05 09:56:56 +0200

    Change exercise. Proper dynamic algorithm copied from http://exercism.io/submissions/fb3d370557054640aa0f6bb87fa83b38

--- change/src/change.clj
+++ change/src/change.clj
@@ -1,31 +1,20 @@
 (ns change)
 
-
-(defn- check-and-sort [used-coins expected-total]
-  (if (= (apply + used-coins)
-         expected-total)
-    (sort used-coins)
-    (throw (IllegalArgumentException. "cannot change"))))
-
 (defn issue
   "Returns proper number of each coin to be used to represent
   total amount"
   [total coins]
-  (let [sorted-coins (->> coins sort reverse)]
-    (loop [remaining-total total
-           used-coins []
-           coins sorted-coins]
-      (if-let [biggest-coin (first coins)]
-        (if (>= remaining-total biggest-coin)
-          (let [biggest-coins-count (quot remaining-total biggest-coin)
-                subtract-amount (* biggest-coin biggest-coins-count)]
-            (recur (- remaining-total subtract-amount)
-                   (into used-coins (repeat biggest-coins-count biggest-coin))
-                   (rest coins)))
-          (recur remaining-total
-                 used-coins
-                 (rest cois)))
-        (check-and-sort used-coins total)))))
+  (let [sorted-coins (sort coins)]
+    (-> (reduce
+         (fn [z x]
+           (->> (map #(conj (z (- x %)) %) sorted-coins)
+                (filter vector?)
+                (reduce (fn ([] nil) ([z x] (min-key count z x))))
+                (assoc z x)))
+         {0 []}
+         (range 1 (inc total)))
+        (get total)
+        (or (throw (IllegalArgumentException. "cannot change"))))))
 
 (issue 25 #{1 5 10 25 100})
 (issue 23 #{1 4 15 20 50})
